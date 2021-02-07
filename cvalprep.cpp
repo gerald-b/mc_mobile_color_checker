af9a23f3e70312c6875d9337899d0102f80e9f11
Gerald Büchler <dev@genomast.de>
2021-02-07 22:20:54 +0100

    Added empty class for RGB-Value Preparation

---
+++ cvalprep.cpp
@@ -0,0 +1,9 @@
+#include "cvalprep.hpp"
+
+CValPrep::CValPrep()
+{
+  this->_rval = 0;
+  this->_gval = 0;
+  this->_bval = 0;
+}
+
---
+++ cvalprep.hpp
@@ -0,0 +1,14 @@
+
+#ifndef C_VAL_PREP_HPP
+#define C_VAL_PREP_HPP
+
+class CValPrep
+{
+private:
+  int _rval;
+  int _gval;
+  int _bval;
+public:
+  CValPrep();
+};
+#endif // C_VAL_PREP_HPP
