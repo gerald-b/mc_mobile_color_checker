31c64b6772114397d56c55aae960d3446b3d90fe
Gerald Büchler <dev@genomast.de>
2021-02-07 22:43:44 +0100

    Added setter & getter methods

--- cvalprep.cpp
+++ cvalprep.cpp
@@ -2,8 +2,43 @@
 
 CValPrep::CValPrep()
 {
-  this->_rval = 0;
-  this->_gval = 0;
-  this->_bval = 0;
+  this->setRVal(0);
+  this->setGVal(0);
+  this->setBVal(0);
 }
 
+void CValPrep::setRVal(int value)
+{
+  if (value < 0) { value = 0; }
+  if (value > 255) { value = 255; }
+  this->_rval = value;
+}
+
+void CValPrep::setGVal(int value)
+{
+  if (value < 0) { value = 0; }
+  if (value > 255) { value = 255; }
+  this->_gval = value;
+}
+
+void CValPrep::setBVal(int value)
+{
+  if (value < 0) { value = 0; }
+  if (value > 255) { value = 255; }
+  this->_bval = value;
+}
+
+int CValPrep::getRVal(void)
+{
+  return this->_rval;
+}
+
+int CValPrep::getGVal(void)
+{
+  return this->_gval;
+}
+
+int CValPrep::getBVal(void)
+{
+  return this->_bval;
+}
--- cvalprep.hpp
+++ cvalprep.hpp
@@ -10,5 +10,13 @@ private:
   int _bval;
 public:
   CValPrep();
+
+  void setRVal(int value);
+  void setGVal(int value);
+  void setBVal(int value);
+
+  int getRVal(void);
+  int getGVal(void);
+  int getBVal(void);
 };
 #endif // C_VAL_PREP_HPP
